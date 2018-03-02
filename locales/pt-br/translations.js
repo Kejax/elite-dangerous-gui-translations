d60d1a59c68feccd593d395ffb6a7f2fedf0e1b7
Gareth Harper <git@spansh.co.uk>
2018-03-02 13:57:04 +0000

    added missing commas

--- locales/pt-br/translations.js
+++ locales/pt-br/translations.js
@@ -29,9 +29,9 @@ export default {
         "warning": "Por favor, certifique-se de que possui um Scanner Detalhado de Superfície antes de embarcar em uma viagem. Caso contrário, suas recompensas serão substancialmente menores do que o esperado."
     },
     "fields_help": {
-        "system_name": "Use este campo para filtrar corpos no sistema com palavras em comum. Ex.: Col resulta em Col 285 Sector IY-W b16-6, mas não Colonia."
-        "body_name": "Use este campo para filtrar por corpos com palavras em comum. Ex.: Sol resultaria em Sol, mas não Solati 6."
-        "reference_system": "Use este campo para indicar a partir de qual sistema você deseja executar a busca."
+        "system_name": "Use este campo para filtrar corpos no sistema com palavras em comum. Ex.: Col resulta em Col 285 Sector IY-W b16-6, mas não Colonia.",
+        "body_name": "Use este campo para filtrar por corpos com palavras em comum. Ex.: Sol resultaria em Sol, mas não Solati 6.",
+        "reference_system": "Use este campo para indicar a partir de qual sistema você deseja executar a busca.",
         "reference_coords": "Use este campo ao invés de Sistema de Referência para indicar a partir de quais coordenadas você deseja executar a busca."
     },
     "fields": {
@@ -56,7 +56,7 @@ export default {
         "system_name": "Nome do Sistema",
         "name": "Nome",
         "reference_coordinates": "Coordenadas de Referência",
-        "reference_system": "Sistema de Referência"
+        "reference_system": "Sistema de Referência",
         "select_reference_system": "Selecione o Sistema de Referência",
         "x_coordinate": "Coordenada X",
         "y_coordinate": "Coordenada Y",
@@ -108,7 +108,7 @@ export default {
         "jumps": "Saltos",
         "jumps_description": "Número estimado de saltos para esse sistema.",
         "terraforming_short": "Terra",
-        "terraforming_description": "Esse corpo é candidato à terraformação?"
+        "terraforming_description": "Esse corpo é candidato à terraformação?",
         "government": "Governo",
         "select_government": "Selecione o Governo",
         "allegiance": "Fidelidade",
@@ -276,17 +276,17 @@ export default {
         "Iron": "Ferro"
     },
     "government": {
-        "None": "Nenhum"
-        "Corporate": "Corporação"
-        "Democracy": "Democracia"
-        "Patronage": "Patrocínio"
-        "Dictatorship": "Ditadura"
-        "Confederacy": "Confederação"
-        "Feudal": "Feudal"
-        "Anarchy": "Anarquia"
-        "Cooperative": "Cooperativa"
-        "Communism": "Comunismo"
-        "Theocracy": "Teocracia"
+        "None": "Nenhum",
+        "Corporate": "Corporação",
+        "Democracy": "Democracia",
+        "Patronage": "Patrocínio",
+        "Dictatorship": "Ditadura",
+        "Confederacy": "Confederação",
+        "Feudal": "Feudal",
+        "Anarchy": "Anarquia",
+        "Cooperative": "Cooperativa",
+        "Communism": "Comunismo",
+        "Theocracy": "Teocracia",
         "Prison Colony": "Colônia Prisional"
     },
     "allegiance": {
